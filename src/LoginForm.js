35380a4b281eae9f1f96739952846a41c1b8304d
Sara Caponi <sbc436@mail.missouri.edu>
2018-10-16 16:50:29 -0500

    login

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 
-export default class LoginForm extends React.Component{
-  render(){
-    return(
-      <h1>Hello from LoginForm!!!!!</h1>
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pawPrint: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.pawPrint.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="LoginForm">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="pawPrint" bsSize="large">
+            <ControlLabel>PawPrint</ControlLabel>
+            <FormControl
+              autoFocus
+              type="pawPrint"
+              value={this.state.pawPrint}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
     );
   }
 }
