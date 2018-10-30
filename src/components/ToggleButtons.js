c409d1928422922ad7a033315ff1ababff458af0
Sara Caponi <sbc436@mail.missouri.edu>
2018-10-30 16:22:53 -0500

    added css for toggleButtons

--- src/components/ToggleButtons.js
+++ src/components/ToggleButtons.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {ButtonToolbar, Button} from 'react-bootstrap';
+import "../styles/ToggleButtons.css";
 
 
 export default class ToggleButtons extends React.Component {
   render(){
     return(
     
-		<div>
+		<div id="buttonDiv">
 		<ButtonToolbar>
     	<Button bsStyle="primary" bsSize="large"> Past Classes </Button>
 		<Button bsStyle="primary" bsSize="large"> Current Classes </Button>
