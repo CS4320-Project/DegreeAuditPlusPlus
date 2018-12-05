3f7adec2d6cfd02aba98ed5427f9154e34e1989a
Derek Rogers <derek.rogers@outlook.com>
2018-12-05 17:57:32 -0600

    gotta blast

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,7 +55,7 @@ export default class Home extends React.Component{
 		</div>
 		<div className="InfoDiv">
             <UserInfo student={this.props.student} />
-            <GPACalculator/>
+            <GPACalculator student={this.props.student}/>
 		</div>
 
 
